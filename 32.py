fdfeaaef2c5599f0f718242b21f45e939c6f6085
Fianery <48380517+Fianery@users.noreply.github.com>
2019-05-10 10:55:24 +0200

    Update 32.py

--- 32.py
+++ 32.py
@@ -1,11 +1,11 @@
 out_file=open("piramis.txt","w")
 n=int(input("szam: "))
-for i in range(0,n+1):
-    for j in range(0,n-i+1):
-        print(end=" ")
-    for j in range(i,0,-1):
+for i in range(1,n+1):
+    for j in range(1,n-i+1):                #ures helyek
+        print(end=" ",file=out_file)
+    for j in range(i,0,-1):                 #ertekek
         print(j,end="",file=out_file)
-    for j in range(2,i+1):
+    for j in range(2,i+1):                  #jobb
         print(j,end="",file=out_file)
     print("\n",file=out_file)
 out_file.close()
